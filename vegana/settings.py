4aeed6034cc8ec24de9e021bb8d7ef8c1ddee091
intellect <odetech@gmail.com>
2020-12-04 12:25:07 +0100

    i setup postgresql database

--- vegana/settings.py
+++ vegana/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'vegana.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vegana-database',
+        'USER': 'intellect',
+        'PASSWORD': 'olatunde98',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
